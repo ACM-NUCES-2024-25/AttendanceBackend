f23c310fdbda3114e1d0f6081da5e0f5b4a281aa
mufeezhanif <mufeez.hanif123@gmail.com>
2024-10-28 02:07:42 +0500

    Not verified, getting errors

--- Controller/MarkAttendanceController.js
+++ Controller/MarkAttendanceController.js
@@ -5,8 +5,65 @@ const Event = require("../Model/EventModel.js");
 
 const { DistanceCheck } = require("../Utils/MarkAttendanceUtils.js");
 
-exports.MarkTeamAttendance = catchAsyncError(async (req , res , next) => {
-    res.status(200).json({
-        success: true
-    });
+exports.MarkTeamAttendance = catchAsyncError(async (req, res, next) => {
+    //fetching location of user
+    const { code, latitude, longitude } = req.body;
+
+    if (!latitude || !longitude) {
+        return next(
+            new ErrorHandler("Error: Please enable location services.", 400)
+        );
+    }
+
+    try {
+        //fetching participant from the database
+        const doc = await MarkAttendance.findOne({ att_code: code });
+        if (!doc) {
+            return next(new ErrorHandler("Error: Entry not Found.", 404));
+        }
+        //checking the competition
+        const eventDetails = await Event.findOne({
+            competitionName: doc.Competition,
+        });
+        if (!eventDetails) {
+            return next(new ErrorHandler("Error: Competition details not found.", 404));
+        }
+
+        //validating if the user is in university or not
+        const distance = DistanceCheck(latitude, longitude);
+        if (distance > 1000) {
+            return next(new ErrorHandler("Error: You are not in the vicinity of the event.", 403));
+        }
+
+        // validating if the competition is going on or not
+
+        const currentTime = new Date(); // This would not work, the current time should be passed by the user's device
+        const eventStartTime = new Date(eventDetails.start_time);
+        const eventEndTime = new Date(eventDetails.end_time);
+
+        if (currentTime < eventStartTime || currentTime > eventEndTime) {
+            return next(new ErrorHandler("Error: Competition has not started or has ended.", 403));
+        }
+        //if it was previously marked
+        if (doc.attendance) {
+            
+            return res
+                .status(200)
+                .json({ message: "Your attendance has already been marked." });
+        }
+
+        doc.attendance = true;
+        await doc.save();
+
+        return res.status(200).json({
+            success: true,
+            message: "Your attendance has been marked.",
+            teamName: doc.Team_Name,
+        });
+    } catch (error) {
+        console.log(error);
+        return res
+            .status(500)
+            .json({ message: "Internal server error", error: error.message });
+    }
 });
--- Utils/MarkAttendanceUtils.js
+++ Utils/MarkAttendanceUtils.js
@@ -1,3 +1,18 @@
-exports.DistanceCheck = () => {
-    console.log("Hello World!!");
-}
+const lat2 = process.env.LATITUDE;
+const lon2 = process.env.LONGITUDE;
+
+exports.DistanceCheck = (lat1, lon1) => {
+  const toRad = (value) => (value * Math.PI) / 180;
+
+  const R = 6371;
+  const dLat = toRad(lat2 - lat1);
+  const dLon = toRad(lon2 - lon1);
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(toRad(lat1)) *
+      Math.cos(toRad(lat2)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  return R * c * 1000;
+};
