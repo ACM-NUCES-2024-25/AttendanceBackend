965b568bd6dd46b2d87b038892fc1cdb3c7b6aef
Azaan <shahazaan32@gmail.com>
2024-11-01 21:01:20 +0500

    Create The Events Apis

--- Controller/AdminController.js
+++ Controller/AdminController.js
@@ -25,16 +25,36 @@ exports.UpdateTeamAttendance = catchAsyncError(async (req , res , next) => {
     });
 });
 
-exports.GetEventDetails = catchAsyncError(async (req , res , next) => {
+exports.GetEventDetails = catchAsyncError(async (req, res, next) => {
+    const { competitionName } = req.body; // Get competition name from request body
+    const event = await Event.find({});
+
+    if (!event) {
+        return next(new ErrorHandler("Event not found", 404));
+    }
+
     res.status(200).json({
         success: true,
-        message: "Get Event Details"
+        event,
     });
 });
 
-exports.UpdateEventTime = catchAsyncError(async (req , res , next) => {
+exports.UpdateEventTime = catchAsyncError(async (req, res, next) => {
+    const { competitionName, start_time, end_time } = req.body; // Get from request body
+
+    // Find the event by competition name and update the times
+    const updatedEvent = await Event.findOneAndUpdate(
+        { competitionName },
+        { start_time, end_time },
+        { new: true, runValidators: true } // Returns the updated document and applies validators
+    );
+
+    if (!updatedEvent) {
+        return next(new ErrorHandler("Event not found", 404));
+    }
+
     res.status(200).json({
         success: true,
-        message: "Update Event Time"
+        updatedEvent,
     });
 });
